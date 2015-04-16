367c49f6ea47ebfe71319c4aee4dcb47f03e6ff4
Soulou <leo@unbekandt.eu>
2015-04-16 21:02:10 +0200

    custom welcome view

---
+++ resources/views/welcome.php
@@ -0,0 +1,52 @@
+<html>
+    <head>
+        <title>Lumen</title>
+        <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+        <style>
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                height: 100%;
+                color: #B0BEC5;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+                margin-bottom: 40px;
+            }
+            .hosted {
+                font-size: 60px;
+                margin-bottom: 40px;
+            }
+
+            .quote {
+                font-size: 24px;
+            }
+        </style>
+    </head>
+    <body>
+        <a href="https://github.com/Scalingo/sample-php-lumen"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub"></a>
+        <div class="container">
+            <div class="content">
+                <div class="title">Lumen.</div>
+                <div class="hosted">Hosted on</div>
+                <div class="scalingo"><a href="https://scalingo.com"><img src="https://scalingo.com/scalingo_logo.png" /></a></div>
+            </div>
+        </div>
+    </body>
+</html>
